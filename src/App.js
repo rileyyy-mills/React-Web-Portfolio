e390173bc9f9d2d8f5ab9b3d45656b71e266eed6
Riley Mills <100374360+rileyyy-mills@users.noreply.github.com>
2024-07-25 14:48:28 -0400

    Updated CSS styles + assets

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
-import './assets/styles/App.css';
+import logo from './assets/imgs/pop-art/pop-art-background.png';
+import './assets/styles/styles.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <div className="banner">
+        <img src={logo} alt="logo" />
+        </div>
       </header>
     </div>
   );
